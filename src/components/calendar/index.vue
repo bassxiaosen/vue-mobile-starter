73129e8075d965907aa923bc69fcdab8dca46738
BassXiaoSen <314121378@qq.com>
2019-09-16 11:29:48 +0800

    添加切换月份功能

--- src/components/calendar/index.vue
+++ src/components/calendar/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="calendarWrapper">
     <div class="navBar">
-      <span>左</span>
-      <span>{{currentYear}}</span>
-      <span>右</span>
+      <span @click="prevMonth">&lt;</span>
+      <span>{{currentYear}}年{{currentMonth+1}}月</span>
+      <span @click="nextMonth">&gt;</span>
     </div>
     <div class="dayWrapper">
       <span>星期一</span>
@@ -15,7 +15,7 @@
       <span>星期日</span>
     </div>
     <div class="calendarContent">
-      <span :style="!item.isThisMonth?`color:#eee`:''" v-for="item in calendarDataArr" :key="item.key">
+      <span :style="!item.isThisMonth?`color:#c0c0c0`:''" v-for="item in calendarDataArr" :key="item.key">
         {{item.key}}
       </span>
     </div>
@@ -24,8 +24,13 @@
 <script>
 export default {
   mounted() {
+    // 初始化年月日数据
     this.today = new Date()
     this.currentYear = this.today.getFullYear()
+    this.currentMonth = this.today.getMonth()
+    this.currentDate = this.today.getDate()
+    console.log(this.$data)
+    // 业务逻辑操作方法
     this.init()
     console.log(this.calendarDataArr)
   },
@@ -33,54 +38,70 @@ export default {
     return {
       today: {},
       currentYear: 0,
+      currentMonth: 0, // 注意是从0开始 0代表1月以此类推
+      currentDate: 0,
       calendarDataArr: []
     }
   },
   methods: {
     // 获取当前月最大天数
-    getCurrentMonthMaxDay() {
-      const today = new Date()
-      return new Date(today.getFullYear(), today.getMonth() + 1, 0).getDate()
+    getMonthMaxDate(year, month) {
+      return new Date(year, month + 1, 0).getDate()
     },
     // 获取当前月第一天是周几
-    getCurrentMonthFirstDay() {
-      const today = new Date()
-      return new Date(today.getFullYear(), today.getMonth(), 1).getDay()
+    getMonthFirstDay(year, month) {
+      return new Date(year, month, 1).getDay()
     },
     // 获取当前月最后一天是周几
-    getCurrentMonthLastDay() {
-      const today = new Date()
-      return new Date(today.getFullYear(), today.getMonth()+1, 0).getDay()
+    getMonthLastDay(year, month) {
+      return new Date(year, month + 1, 0).getDay()
+    },
+    // 上下月切换逻辑
+    prevMonth() {
+      this.switchMonthOperation('prev')
+    },
+    nextMonth() {
+      this.switchMonthOperation('next')
+    },
+    switchMonthOperation(type) {
+      // 月份切换逻辑
+      if(type === 'prev') {
+        this.currentMonth = this.currentMonth - 1
+      } else if(type === 'next') {
+        this.currentMonth = this.currentMonth + 1
+      }
+      const thatMonth = new Date(this.currentYear, this.currentMonth)
+      // 重新赋值
+      this.currentYear = thatMonth.getFullYear()
+      this.currentMonth = thatMonth.getMonth()
+      this.init()
     },
     // 处理逻辑
     init() {
-      const maxDate = this.getCurrentMonthMaxDay()
-      let firstDay = this.getCurrentMonthFirstDay()
-      let lastDay = this.getCurrentMonthLastDay()
+      this.calendarDataArr = []
+      const maxDate = this.getMonthMaxDate(this.currentYear, this.currentMonth)
+      let firstDay = this.getMonthFirstDay(this.currentYear, this.currentMonth)
+      let lastDay = this.getMonthLastDay(this.currentYear, this.currentMonth)
       // 先判断星期几进行补位，先进行星期天简化处理
       firstDay = firstDay === 0 ? 7 : firstDay
       lastDay = lastDay === 0 ? 7 : lastDay
       let count = firstDay - 1 // 需要往前补几个
       for(let i = count; i > 0; i--) {
-        const today = new Date()
-        const thatDay = new Date(today.getFullYear(), today.getMonth(), -(i-1))
+        const thatDay = new Date(this.currentYear, this.currentMonth, -(i-1))
         this.calendarDataArr.push({
-          key: `${thatDay.getMonth()+1}-${thatDay.getDate()}`,
+          key: `${thatDay.getMonth() + 1}-${thatDay.getDate()}`,
           isThisMonth: false,
         })
       }
       for(let i = 1; i <= maxDate; i++) {
-        const today = new Date()
         this.calendarDataArr.push({
-          // key: `${today.getFullYear()}-${today.getMonth()+1}-${i}`
-          key: `${today.getMonth()+1}-${i}`,
+          key: `${this.currentMonth + 1}-${i}`,
           isThisMonth: true,
         })
       }
       let lastCount = 7 - lastDay
       for(let i = 1; i <= lastCount; i++) {
-        const today = new Date()
-        const thatDay = new Date(today.getFullYear(), today.getMonth()+1, i)
+        const thatDay = new Date(this.currentYear, this.currentMonth + 1, i)
         this.calendarDataArr.push({
           key: `${thatDay.getMonth()+1}-${thatDay.getDate()}`,
           isThisMonth: false,
